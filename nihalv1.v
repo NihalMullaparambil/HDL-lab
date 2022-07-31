1c47c259598c971a797f3a83900b340fc90815d7
NihalMullaparambil <105635486+NihalMullaparambil@users.noreply.github.com>
2022-07-31 15:31:46 +0200

    Add files via upload

--- nihalv1.v
+++ nihalv1.v
@@ -4,7 +4,7 @@ module Controller2 (
  input rst_n ,
  input [7:0] ADC,
  output reg [6:0] DC_Comp,
- output reg LED_IR , LED_RED,
+ output reg LED_IR , LED_RED ,
  output reg [3:0] PGA_Gain) ;
  reg [6:0] DC_RED, DC_IR; 
  reg [3:0] PGA_RED ,PGA_IR;
@@ -12,6 +12,7 @@ module Controller2 (
 //nihal code declaration begins 
 reg [3:0] StateOfMachine;
 reg [7:0] errorDc;
+reg measureOrControl;
 
 //Nihal local param
  localparam [2:0]
@@ -27,10 +28,10 @@ always@( posedge clk or negedge rst_n ) begin
  begin
 	 // Nihal reset signals starts
 	 StateOfMachine <= find_DC_comp_IR;
-	 DC_Comp <= 64;
+	 DC_Comp <= 50;
 	 DC_IR <= 0;
-	 errorDc <= 0;
-     halfError <= 0;     
+	 errorDc <= 3;
+	 measureOrControl = 1;
  end
  else 
  begin
@@ -38,43 +39,47 @@ always@( posedge clk or negedge rst_n ) begin
 	default : begin
 		//restart from begning
 		StateOfMachine <= find_DC_comp_IR;
-		DC_Comp <= 64;
-		DC_IR <= 0;
-		errorDc <= 0;
-        halfError <= 0;  	
+		DC_Comp <= 40;
+		DC_IR <= 1;
+		errorDc <= 3;
+		measureOrControl = 1;
 	end
 	find_DC_comp_IR : 
 	begin
-		if(ADC > 8'b01111111)
-        begin
-            errorDc <= ADC - 8'b01111111 ;
-            halfError <= errorDc/2;
-        end
-        else
-        begin
-    	    errorDc <= 8'b01111111 - ADC;
-            halfError <= errorDc/2;
-        end
-		if((ADC == 8'b01111111)||errorDc < 8'b00000010)
+		if(measureOrControl)
 		begin
-			StateOfMachine <= find_PGA_comp_IR;
-			DC_IR <= DC_Comp;
-			DC_Comp <= 64;
+			if(ADC > 8'b01111111)
+			begin
+				errorDc <= ADC - 8'b01111111 ;
+			end
+			else
+			begin
+				errorDc <= 8'b01111111 - ADC;
+			end
+			if((ADC == 8'b01111111)||errorDc < 8'b00000011)
+			begin
+				StateOfMachine <= find_PGA_comp_IR;
+				DC_IR <= DC_Comp;
+				DC_Comp <= 64;
+			end
+			measureOrControl <= !measureOrControl;
 		end
 		else
 		begin
 			DC_IR <= 0;
 			StateOfMachine <= find_DC_comp_IR;
 			if(ADC > 8'b01111111)
-				DC_Comp <=  DC_Comp + halfError;
+				DC_Comp <=  DC_Comp + (errorDc/2);
 			else
-                if(halfError > DC_Comp)
-                	DC_Comp <=  halfError - DC_Comp;
-                else
-                    DC_Comp <=  DC_Comp - halfError;
-
+				if((errorDc/2) > DC_Comp)
+					DC_Comp <=  (errorDc/2) - DC_Comp;
+				else 
+					DC_Comp <=  DC_Comp - (errorDc/2) ;
+			measureOrControl <= !measureOrControl;
 		end
 	end
+	find_DC_comp_IR: 	StateOfMachine <= find_DC_comp_IR;
+	
 	endcase
 end
 	
